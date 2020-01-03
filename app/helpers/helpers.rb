1e853d1e55b2e588e4af4f9e14db0aebaf87c505
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2020-01-03 21:59:50 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,13 +1,10 @@
 class Helpers
   def self.current_user(session_hash)
-    # user = User.find(session_hash[:user_id])
-    
-    session_hash[:user_id] = @user.id
-
+    user = User.find(session_hash[:user_id])
   end
   
   def self.is_logged_in?(session_hash)
     # double negation forces the return value to be a boolean
-    !!self.current_user == session_hash[:user_id]
+    !!session_hash[:user_id]
   end
 end
